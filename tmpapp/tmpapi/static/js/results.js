4bd8adf7fb2245e7b59918bda77281ed01638a2b
_bartek <barteksielicki@gmail.com>
2016-09-25 15:21:02 +0200

    final front?

--- tmpapp/tmpapi/static/js/results.js
+++ tmpapp/tmpapi/static/js/results.js
@@ -1,50 +1,63 @@
 $(function() {
+    /* autoslide on enter */
+    setTimeout(function() {
+        $('html, body').animate({
+            scrollTop: $('main').offset().top
+        }, 500);
+    }, 1000);
+
+    /* mapster */
     var inArea;
+    var current;
     var map = $('.map-image');
     var single_opts = {
-                stroke: true,
-                strokeColor: 'ff0000',
-                strokeWidth: 5
-            },
-            all_opts = {
-                fillColor: 'd0d0d0',
-                fillOpacity: 0.2,
-                stroke: true,
-                strokeWidth: 5,
-                strokeColor: 'ff0000',
-                strokeOpacity: 0.4
-            },
-            initial_opts = {
-                mapKey: 'data-name',
-                isSelectable: false,
-                onMouseover: function (data) {
-                    inArea = true;
-                    var self = $(this);
-                    var clause_id = self.data('clause');
-                    var clause_div = $('.clause[data-clause='+clause_id+']');
-                    clause_div.show();
-                },
-                onMouseout: function (data) {
-                    inArea = false;
-                    var self = $(this);
-                    var clause_id = self.data('clause');
-                    var clause_div = $('.clause[data-clause='+clause_id+']');
-                    clause_div.hide();
-                }
-            };
+            stroke: true,
+            strokeColor: '1abc9c',
+            strokeWidth: 4
+        };
+    var all_opts = {
+            fillColor: 'd0d0d0',
+            fillOpacity: 0.2,
+            stroke: true,
+            strokeWidth: 4,
+            strokeColor: '1abc9c',
+            strokeOpacity: 0.75
+    };
+    var initial_opts = {
+        mapKey: 'data-name',
+        isSelectable: false,
+    };
+    var opts = $.extend({}, all_opts, initial_opts, single_opts);
+    map.mapster(opts);
 
-    opts = $.extend({}, all_opts, initial_opts, single_opts);
-    map.mapster('unbind')
-            .mapster(opts)
-            .bind('mouseover', function () {
-                if (!inArea) {
-                    map.mapster('set_options', all_opts)
-                            .mapster('set', true, 'all')
-                            .mapster('set_options', single_opts);
-                }
-            }).bind('mouseout', function () {
-        if (!inArea) {
-            map.mapster('set', false, 'all');
-        }
+    /* using mapster */
+    $('.clause').on('mouseover', function() {
+        var self = $(this);
+        var clause_id = self.data('clause');
+        current = clause_id.toString();
+        map.mapster('set_options', all_opts)
+                       .mapster('set', true, current)
+                       .mapster('set_options', single_opts);
+    });
+    $('.clause').on('mouseout', function() {
+        map.mapster('set', false, current);
+        console.log("out");
+    });
+    
+    /* fixed on scroll */
+    var semi_fixed_item = $('.clauses-col');
+    var offset = semi_fixed_item.offset().top;
+    var left_col_height = $('.image-col').height();
+    $(window).scroll(function() {
+        //console.log($(window).scrollTop());
+        //console.log(semi_fixed_item.offset.top);
+        if($(window).scrollTop() > offset) {
+            var margin = $(window).scrollTop() - offset;
+            if (margin + semi_fixed_item.height() <= left_col_height) {
+                semi_fixed_item.css('margin-top',margin+'px');
+            }
+        } else {
+            semi_fixed_item.css('margin-top','0');
+        }  
     });
 });
