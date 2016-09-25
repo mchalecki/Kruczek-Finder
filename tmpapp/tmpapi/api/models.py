1a5285a0eaa40e1e21986a0dfbe5732643d55c7a
Marcin Chalecki <marcin.ch@webinterpret.com>
2016-09-25 02:06:39 +0200

    Clauses model.

--- tmpapp/tmpapi/api/models.py
+++ tmpapp/tmpapi/api/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+
+class Clauses:
+    data_wydania = models.DateField()
+    sygnatura_akt = models.CharField(max_length=40)
+    nazwa_i_siedziba_sądu_który_wydal_wyrok = models.CharField(max_length=100)
+    powod = models.CharField(max_length=100)
+    pozwani = models.CharField(max_length=100)
+    postanowienie_wzorca = models.TextField()
+    data_dokonania_wpisu = models.DateField()
+    uwagi = models.TextField()
+    branza = models.CharField(max_length=60)
