89252b57c21776e0a06710ff06f85bf6b02c262b
Marcin Chalecki <marcin.ch@webinterpret.com>
2016-09-25 03:17:49 +0200

    More models. Request Session and Image models.

--- tmpapp/tmpapi/api/models.py
+++ tmpapp/tmpapi/api/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from datetime import timedelta, datetime
 
 
-class Clauses:
+class Clauses(models.Model):
     data_wydania = models.DateField()
     sygnatura_akt = models.CharField(max_length=40)
     nazwa_i_siedziba_sądu_który_wydal_wyrok = models.CharField(max_length=100)
@@ -11,3 +12,15 @@ class Clauses:
     data_dokonania_wpisu = models.DateField()
     uwagi = models.TextField()
     branza = models.CharField(max_length=60)
+
+
+class Request_Session(models.Model):
+    token = models.CharField(max_length=40)
+    email = models.EmailField()
+
+
+class Image(models.Model):
+    request_session = models.ForeignKey(Request_Session, on_delete=models.CASCADE)
+    image = models.ImageField()
+    url = models.CharField(max_length=40)
+    expire_date = models.DateTimeField(default=datetime.today() + timedelta(days=7))
